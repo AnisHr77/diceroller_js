1d0a3fa570e576c7bde27a7c0abd79d25731b140
Anis HADJ RAMDANE <anis28hr@gmail.com>
2025-03-22 23:21:53 +0100

    Initial commit

---
+++ .idea/index.js
@@ -0,0 +1,20 @@
+function rollDice(){
+    const numberOfDices = document.getElementById("numberOfDices").value;
+    const textResult = document.getElementById("text-result");
+    const images = document.getElementById("images-result");
+
+    const ruslts = [] ;
+    const rusltimg = [] ;
+
+
+
+    for (let i = 0; i < numberOfDices; i++)
+    {
+        const random = Math.floor(Math.random() * 6)+1;
+        ruslts.push(random) ;
+        rusltimg.push(`<img src ="../dice_images/${random}.png">`) ;
+
+    }
+    textResult.textContent=` La resultat de roller : ${ruslts.join(",")}` ;
+    images.innerHTML= rusltimg.join('') ;
+}
