2f00b948202bb11a0b827daa6cebe1f1af0b6cdd
Anis Hadj Ramdane <anis28hr@gmail.com>
2025-03-23 22:38:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,39 @@ for(let i=0 ; i<ruslts.length ; i++)
     }
 console.log("this is for jota");
 
+function generatePassword(length ,ContainLowerCase,ContainUpperCase,ContainSymbols,ContainNumbers ){
+const password="";
+const Concatination ="";
+const passLowerCase = "abcdefghijklmnopqrstuvwxyz" ;
+const passUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+const PassSymbols ="<>.,/*()_+*(&^%$#@!"  ;
+const passNumbers = '0123456789"
+
+    Concatination+=ContainLowerCase?passLowerCase :"" ;
+     Concatination+=ContainUpperCase?passUpperCase :"" ;
+     Concatination+=ContainSymbols?PassSymbols :"" ;
+     Concatination+=ContainNumbers?passNumbers :"" ;
+
+    if (length<8){
+    return "password must have at least 8 characteres"
+    }
+    {
+        for( let j=0 ; j<length ; j++)
+            {
+                const AuHasard= Math.floor(Math.random()*Concatination.length) ; 
+                password+=Concatination[AuHasard];
+}
+        return password ;
+    }
+const length=12 ;
+const ContainLowerCase=true;
+const ContainUpperCase=true;
+const ContainSymbols=true;
+const ContainNumbers=true;
+
+  
+
+
 
 
 
